5c1ba0c5da5db3cd30f2eed65670865ee737c795
Alexander <alexander.pankov88@mail.ru>
2021-04-18 14:11:36 +0700

    добавил шаблоны main.php и layout.php

--- helpers.php
+++ helpers.php
@@ -96,6 +96,7 @@ function db_get_prepare_stmt($link, $sql, $data = []) {
  *
  * @return string Рассчитанная форма множественнго числа
  */
+
 function get_noun_plural_form (int $number, string $one, string $two, string $many): string
 {
     $number = (int) $number;
